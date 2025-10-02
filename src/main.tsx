b317c6231e0c077acb54415bdd6915c319db6beb
Ross Whitehouse <ross@MacBookAir.lan>
2025-10-02 18:05:07 +0100

    Remove about route.

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from './App.tsx'
-import About from './routes/About.tsx';
 import LetterboxdImgGenerator from './routes/LetterboxdImgGenerator.tsx';
 import Layout from './components/Layout.tsx';
 import './index.css'
@@ -13,7 +12,6 @@ createRoot(document.getElementById('root')!).render(
         <Layout>
           <Routes>
             <Route path="/" element={<App />} />
-            <Route path="/about" element={<About />} />
             <Route path="/letterboxd-img" element={<LetterboxdImgGenerator />} />
           </Routes>
         </Layout>
